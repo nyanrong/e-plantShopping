32d4c354560c6589334d232e80107f51faed8493
nyanrong <nyrlynette@gmail.com>
2025-02-23 13:01:21 -0500

    Task 2

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -2,7 +2,7 @@ import React, { useState,useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
 
-import { addItem, removeItem, updateQuantity} from "./CartSlice";
+import { addItem } from "./CartSlice";
 
 
 function ProductList() {
