5275ec4e47338894bfe27b4c6a8ef687091e5057
nyanrong <nyrlynette@gmail.com>
2025-02-24 10:10:29 -0500

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,9 @@ function App() {
         <div className="background-image"></div>
         <div className="content">
          <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
+         <h1>Welcome To The Greenery Store</h1>
           <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
+          <p>Where Nature Greets You</p>
          
           <button className="get-started-button" onClick={handleGetStartedClick}>
             Get Started
